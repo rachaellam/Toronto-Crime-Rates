473134e93ee946b771f90174c397748f6f7f1a88
Rachael Lam <rachael.a.lam@gmail.com>
2021-01-26 17:09:17 -0800

    Unsaved update

--- scripts/00_toronto_crime_data_import.R
+++ scripts/00_toronto_crime_data_import.R
@@ -1,5 +1,5 @@
 ### Preamble ###
-# Purpose: Clean the Neighbourhood Crime Rates data downloaded from Toronot Open Data
+# Purpose: Clean the Neighbourhood Crime Rates data downloaded from Toronto Open Data
 # Author: Rachael Lam
 # Date: January 19 2021
 # Contact: rachael.lam@mail.utoronto.ca
@@ -30,6 +30,7 @@ view(all_data)
 
 ### Saving the dataset ###
 write_csv(raw_data, "inputs/data/raw_data.csv")
+raw_data <- read.csv("inputs/data/raw_data.csv")
 
 ### Cleaning the raw data ###
 toronto_crime <- raw_data %>%
@@ -55,7 +56,7 @@ toronto_crime <-
 
 head(toronto_crime, 10) # checking first 10 rows to make sure the years were mutated.
 
-### Graphing robbery rates ###
+### Graphing robbery rates of 16 neighbourhoods in the downtown core ###
 toronto_crime %>%
   filter(Neighbourhood %in% c("Niagra", "South Parkdale", "Little Portugal", 
                               "Roncesvalles", "Dufferin Grove", "Trinity-Bellwoods", 
@@ -72,12 +73,12 @@ toronto_crime %>%
        subtitle = "Number of robberies per year") +
   theme_minimal()
 
-### Finding top neighbourhood robbery rates ###
+### Finding the neighbourhoods with the highest robbery average and plotting the crime numbers over time ###
 toronto_crime_highest <- raw_data %>%
   as_tibble() %>%
   select(Neighbourhood, Robbery_2014, Robbery_2015, Robbery_2016, Robbery_2017, Robbery_2018, Robbery_2019, Robbery_AVG) %>% # focusing on robbery. There are other crimes to choose from if desired.
   slice_max(Robbery_AVG, n = 10) %>% # choosing 10 neighbourhoods with the highest robbery average between 2014-2019
-  select(Neighbourhood, Robbery_2014, Robbery_2015, Robbery_2016, Robbery_2017, Robbery_2018, Robbery_2019) %>%
+  select(Neighbourhood, Robbery_2014, Robbery_2015, Robbery_2016, Robbery_2017, Robbery_2018, Robbery_2019) %>% # removing Robber_AVG column
   melt(id = c("Neighbourhood"), #using `reshape2` to change dataframe to long-format data for plotting. kept Neighbourhood as the ID
        variable.name = "Year",
        value.name = "Number_of_Robberies")
@@ -110,7 +111,7 @@ toronto_crime_highest%>%
 ### Graphing neighbourhoods with the largest populations ###
 toronto_crime_pop_top <- raw_data %>%
   as_tibble() %>%
-  select(Neighbourhood, Population) %>% # focusing on robbery. There are other crimes to choose from if desired.
+  select(Neighbourhood, Population) %>%
   slice_max(Population, n = 10)
 
 toronto_crime_pop_top %>%
@@ -120,8 +121,9 @@ toronto_crime_pop_top %>%
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
-### All crime ###
+### Graphing neighbourhoods with the highest crime numbers and their population ###
 toronto_crime_all <- raw_data %>%
+  as_tibble() %>%
   select(Neighbourhood,
          Assault_2014, Assault_2015, Assault_2016, Assault_2017, Assault_2018, Assault_2019,
          AutoTheft_2014, AutoTheft_2015, AutoTheft_2016, AutoTheft_2017, AutoTheft_2018, AutoTheft_2019,
@@ -131,27 +133,26 @@ toronto_crime_all <- raw_data %>%
          Robbery_2014, Robbery_2015, Robbery_2016, Robbery_2017, Robbery_2018, Robbery_2019) %>%
   melt(id = c("Neighbourhood"), #using `reshape2` to change dataframe to long-format data for plotting. kept Neighbourhood as the ID
        variable.name = "Year",
-       value.name = "Number_of_Crimes")
-
-toronto_crime_all %>%
+       value.name = "Number_of_Crimes") %>%
   group_by(Neighbourhood) %>%
-  summarise(Number_of_Crimes = sum(Number_of_Crimes)) %>%
-  slice_max(Number_of_Crimes, n = 10)
+  summarise(Number_of_Crimes = sum(Number_of_Crimes)) %>% # summing crime by neighbourhood 
+  slice_max(Number_of_Crimes, n = 10) # selecting top 10 neighbourhoods with the most crime
 
 toronto_crime_pop <- raw_data %>%
-  select(Neighbourhood, Population) %>% # focusing on robbery. There are other crimes to choose from if desired.
+  as_tibble() %>%
+  select(Neighbourhood, Population) %>%
   filter(Neighbourhood %in% c("Waterfront Communities-The Island", "Bay Street Corridor", "Church-Yonge Corridor", 
                               "West Humber-Clairville", "Moss Park", "York University Heights",
-                              "Downsview-Roding-CFB", "Kensington-Chinatown", "Woburn", "West Hill"))
+                              "Downsview-Roding-CFB", "Kensington-Chinatown", "Woburn", "West Hill")) # grabbing the population of the top 10 neighbourhoods with the most crime
 
+compare <- merge(x = toronto_crime_all, y = toronto_crime_pop, by = "Neighbourhood", all = TRUE) # merging the population and crime dataframes
+compare %>% # plotting the number of crimes with their population
+  ggplot(aes(x = Neighbourhood, y = Number_of_Crimes, fill = Population)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  labs(x = "Neighbourhood",
+       y = "Number of Crimes Committed",
+       title = "Toronto Crime Rate",
+       subtitle = "Top 10 neighbourhoods with the most crimes between 2014-2019") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-### Assault_Rate ###
-toronto_crime_rate <- raw_data %>%
-  select(Neighbourhood, Robbery_Rate_2019)
-
-toronto_crime_rate$Robbery_Rate_2019 <- as.integer(toronto_crime_rate$Robbery_Rate_2019)
-
-toronto_crime_rate %>%
-  slice_max(Robbery_Rate_2019, n = 10)
-toronto_crime_rate
+### Compare
